adb0d68df21d2b1e1c53f204bc20a10e2717f926
Tradex99 <tradex964@gmail.com>
2026-08-21 20:23:33 +0100

    Add files via upload

--- cookie.py
+++ cookie.py
@@ -4,7 +4,7 @@ import sys
 from playwright.sync_api import sync_playwright
 
 
-URL = "https://trade.infinityfree.io/?i=1"
+URL = "https://arb-bot.infinityfree.io/?i=1"
 USER_AGENT = (
     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
     "(KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36"
